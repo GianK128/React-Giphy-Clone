c48d1bf405500a796bf0191dbf882228a2f9997b
GianK128 <giankeberlein@gmail.com>
2022-05-05 09:04:29 -0300

    Extract gif search to services folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,12 @@
 import React, { useEffect, useState } from 'react'
+import { getGifsBySearch } from '../services/gifsService'
 import './App.css'
-
-const apiKey = `${process.env.REACT_APP_GIPHY_API_KEY}`
-const baseURL = "https://api.giphy.com/v1"
-
-const getGifsSearchService = () => {
-  return fetch(`${baseURL}/gifs/search?api_key=${apiKey}&q=panda&limit=10&offset=0&rating=G&lang=es`)
-    .then(res => res.json())
-    .catch(error => {
-      console.log(error)
-      return []
-    })
-}
 
 function App() {
   const [gifs, setGifs] = useState([])
   
   useEffect(() => {
-    getGifsSearchService()
-      .then(response => {
-        const {data} = response
-        if (Array.isArray(data)) {
-          const gifs = data.map(image => image.images.downsized_medium.url)
-          setGifs(gifs)
-        }
-      })
+    getGifsBySearch().then(newGifs => setGifs(newGifs))
   }, [])
 
   return (
