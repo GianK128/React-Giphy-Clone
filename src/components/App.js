f67a2873973e33daad1c4a36ee33d7b6ede688c8
GianK128 <giankeberlein@gmail.com>
2022-05-05 08:38:38 -0300

    Use the API and render the first gifs

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,36 @@
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
+
+const apiKey = `${process.env.REACT_APP_GIPHY_API_KEY}`
+const baseURL = "https://api.giphy.com/v1"
+
+const getGifsSearchService = () => {
+  return fetch(`${baseURL}/gifs/search?api_key=${apiKey}&q=panda&limit=10&offset=0&rating=G&lang=es`)
+    .then(res => res.json())
+    .catch(error => {
+      console.log(error)
+      return []
+    })
+}
 
 function App() {
+  const [gifs, setGifs] = useState([])
+  
+  useEffect(() => {
+    getGifsSearchService()
+      .then(response => {
+        const {data} = response
+        if (Array.isArray(data)) {
+          const gifs = data.map(image => image.images.downsized_medium.url)
+          setGifs(gifs)
+        }
+      })
+  }, [])
+
   return (
-    <h1>Hello Giphy!</h1>
+    <div className='App'>
+      {gifs.map(gif => <img key={gif} src={gif} alt='Gif' />)}
+    </div>
   );
 }
 
