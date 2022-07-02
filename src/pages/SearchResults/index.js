9f366b704d0328d0abadfb1411502204a80d1e24
GianK128 <giankeberlein@gmail.com>
2022-07-02 20:50:03 -0300

    Do gif searching in SearchResults component

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react"
+import Loader from '../../components/Loader'
 import ListOfGifs from "../../components/ListOfGifs"
+import { getGifsBySearch } from "../../services/gifsService"
 
 export default function SearchResults({ params }) {
     const { keyword } = params
-    return <ListOfGifs keyword={keyword}></ListOfGifs>
+    const [loading, setLoading] = useState(false)
+    const [gifs, setGifs] = useState([])
+
+    useEffect(() => {
+        setLoading(true)
+        getGifsBySearch({ keyword })
+            .then(newGifs => {
+                setGifs(newGifs)
+                setLoading(false)
+            })
+    }, [keyword])
+
+    return <>
+        {loading
+            ? <Loader />
+            : <ListOfGifs gifs={gifs} />
+        }
+    </>
 }
