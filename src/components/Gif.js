f7d5c333f0fa840169404bdd6b80a49c275d507b
GianK128 <giankeberlein@gmail.com>
2022-07-02 20:18:48 -0300

    Add link to gif details page

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from 'wouter'
 
 const Gif = ({ id, title, url }) => {
   return <>
     <h3>{title}</h3>
-    <img src={url} alt={title} />
+    <Link to={`/gifs/${id}`}>
+      <img src={url} alt={title} />
+    </Link>
   </>
 }
 
