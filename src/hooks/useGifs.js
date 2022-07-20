6c19fe9848ce6e35980392ac29732f92826383a6
GianK128 <giankeberlein@gmail.com>
2022-07-20 11:00:32 -0300

    Use localStorage to store last keyword searched

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -1,16 +1,21 @@
 import { useState, useEffect } from "react"
 import { getGifsBySearch } from "../services/gifsService"
 
-const useGifs = ({ keyword }) => {
+const useGifs = ({ keyword } = { keyword: '' }) => {
     const [loading, setLoading] = useState(false)
     const [gifs, setGifs] = useState([])
 
     useEffect(() => {
         setLoading(true)
-        getGifsBySearch({ keyword })
+
+        // Elegir keyword para usar en la busqueda
+        const keywordToUse = keyword || localStorage.getItem('lastKeyword') || 'trending'
+
+        getGifsBySearch({ keyword: keywordToUse })
             .then(newGifs => {
                 setGifs(newGifs)
                 setLoading(false)
+                localStorage.setItem('lastKeyword', keywordToUse)
             })
     }, [keyword])
 
