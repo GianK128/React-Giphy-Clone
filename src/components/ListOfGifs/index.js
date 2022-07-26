543096551615ba676b44be887439a55071d0c23f
GianK128 <giankeberlein@gmail.com>
2022-07-26 12:02:10 -0300

    Fix style of ListOfGifs so lazy-loading works fine

--- src/components/ListOfGifs/index.js
+++ src/components/ListOfGifs/index.js
@@ -1,14 +1,17 @@
 import Gif from "../Gif";
+import './ListOfGifs.css'
 
 const ListOfGifs = ({ gifs }) => {
-  return gifs.map(gif => 
-    <Gif 
-      key={gif.id}
-      id={gif.id}
-      title={gif.title}
-      url={gif.url}
-    />
-  )
+  return <div className="App-ListOfGifs">
+    {gifs.map(gif => 
+      <Gif 
+        key={gif.id}
+        id={gif.id}
+        title={gif.title}
+        url={gif.url}
+      />
+    )}
+  </div>
 }
 
 export default ListOfGifs
