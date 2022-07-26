5891763bf1bdef07aa0e627fa4de6207319eb08d
GianK128 <giankeberlein@gmail.com>
2022-07-26 12:01:18 -0300

    Lazy-load TrendingSearches component always

--- src/components/TrendingSearches/index.js
+++ src/components/TrendingSearches/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import getTrendingTerms from "../../services/trendingTermsService"
 import Category from "../Category"
 
@@ -10,9 +10,39 @@ const TrendingSearches = () => {
       .then((data) => 
         setTrends(data)
       )
-  })
+  }, [])
 
   return <Category name={'Tendencias'} options={trends} />
 }
 
-export default TrendingSearches
+const LazyTrendingSearches = () => {
+  const [isNearScreen, setNearScreen] = useState(false)
+  const elementRef = useRef()
+
+  // Create observer only when first loading
+  useEffect(() => {
+    const onObserved = (entries, observer) => {
+      const element = entries[0]
+      console.log(element)
+      if (element.isIntersecting) {
+        setNearScreen(true)
+        observer.disconnect()
+      }
+    }
+    
+    // Observe element to be lazy-loaded
+    const observer = new IntersectionObserver(onObserved, {
+      rootMargin: '100px'
+    })
+    observer.observe(elementRef.current)
+
+    // Disconnect when de-rendering component
+    return () => observer && observer.disconnect()
+  })
+
+  return <div ref={elementRef}>
+    {isNearScreen ? <TrendingSearches /> : null}
+  </div>
+}
+
+export default LazyTrendingSearches
