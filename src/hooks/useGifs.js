c6f5d9af35069edf541054c6e8ea25c32a6b47a2
GianK128 <giankeberlein@gmail.com>
2022-07-28 00:16:14 -0300

    Add simple pagination in useGifs hook

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -2,15 +2,20 @@ import { useContext, useState, useEffect } from "react"
 import { getGifsBySearch } from "services/gifsService"
 import GifsContext from 'context/GifsContext'
 
+const INITIAL_PAGE = 0
+
 const useGifs = ({ keyword } = { keyword: '' }) => {
     const [loading, setLoading] = useState(false)
+    const [loadingPage, setLoadingPage] = useState(false)
+    const [page, setPage] = useState(INITIAL_PAGE)
     const {gifs, setGifs} = useContext(GifsContext)
 
+    // Pick keyword to use
+    const keywordToUse = keyword || localStorage.getItem('lastKeyword') || 'trending'
+
+    // Keyword loading
     useEffect(() => {
         setLoading(true)
-
-        // Elegir keyword para usar en la busqueda
-        const keywordToUse = keyword || localStorage.getItem('lastKeyword') || 'trending'
 
         getGifsBySearch({ keyword: keywordToUse })
             .then(newGifs => {
@@ -18,9 +23,23 @@ const useGifs = ({ keyword } = { keyword: '' }) => {
                 setLoading(false)
                 localStorage.setItem('lastKeyword', keywordToUse)
             })
-    }, [keyword, setGifs])
+    }, [keywordToUse, setGifs])
 
-    return [loading, gifs]
+    // Pagination
+    useEffect(() => {
+        if (page === INITIAL_PAGE) return
+
+        setLoadingPage(true)
+
+        // Add new gifs to the end
+        getGifsBySearch({ keyword: keywordToUse, page })
+            .then(newGifs => {
+                setGifs(prevGifs => prevGifs.concat(newGifs))
+                setLoadingPage(false)
+            })
+    }, [keywordToUse, setGifs, page])
+
+    return {loading, gifs, loadingPage, setPage}
 }
 
 export default useGifs
