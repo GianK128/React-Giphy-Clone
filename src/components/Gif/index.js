d4144cff7f37bf6ce8c010aa2d833b81233840b6
GianK128 <giankeberlein@gmail.com>
2022-08-06 21:06:19 -0300

    Update CSS of ListOfGifs to show them in a grid

--- src/components/Gif/index.js
+++ src/components/Gif/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from 'wouter'
+import './styles.css'
 
 const Gif = ({ id, title, url }) => {
-  return <>
-    <h3>{title}</h3>
+  return <div className="Gif">
+    <div className="Gif-title">
+      <h3>{title}</h3>
+    </div>
     <Link to={`/gif/${id}`}>
       <img src={url} alt={title} />
     </Link>
-  </>
+  </div>
 }
 
 export default Gif
--- src/components/ListOfGifs/index.js
+++ src/components/ListOfGifs/index.js
@@ -4,12 +4,13 @@ import './styles.css'
 const ListOfGifs = ({ gifs }) => {
   return <div className="ListOfGifs">
     {gifs.map(gif => 
-      <Gif className="ListOfGifs-item"
-        key={gif.id}
-        id={gif.id}
-        title={gif.title}
-        url={gif.url}
-      />
+      <div className="ListOfGifs-item" key={gif.id}>
+        <Gif
+          id={gif.id}
+          title={gif.title}
+          url={gif.url}
+        />
+      </div>
     )}
   </div>
 }
