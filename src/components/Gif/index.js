689edede90c1a84f2b9b0a78213853957164d6ae
GianK128 <giankeberlein@gmail.com>
2022-08-06 21:17:44 -0300

    Fix style of Gif's title

--- src/components/Gif/index.js
+++ src/components/Gif/index.js
@@ -4,10 +4,8 @@ import './styles.css'
 
 const Gif = ({ id, title, url }) => {
   return <div className="Gif">
-    <div className="Gif-title">
-      <h3>{title}</h3>
-    </div>
-    <Link to={`/gif/${id}`}>
+    <Link to={`/gif/${id}`} className="Gif-link">
+    <h3>{title}</h3>
       <img src={url} alt={title} />
     </Link>
   </div>
